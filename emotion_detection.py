7c1c382f1390aa73d11ff2eeb150df937bc1277e
klafke0984 <eklafke@gmail.com>
2026-02-07 21:38:36 -0500

    Step3

--- emotion_detection.py
+++ emotion_detection.py
@@ -12,7 +12,23 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = input_json, headers = header)
 
     if response.status_code == 200:
-        return response.text
+        emotions = response.json()
+
+        emotions_dict = emotions['emotionPredictions'][0]['emotion']
+
+        top_emotion = max(emotions_dict, key = emotions_dict.get)
+
+        emotion = {
+            'anger' : emotions_dict['anger'],
+            'disgust' : emotions_dict['disgust'],
+            'fear' : emotions_dict['fear'],
+            'joy' : emotions_dict['joy'],
+            'sadness' : emotions_dict['sadness'],
+            'dominant_emotion' : top_emotion
+        }
+
+        return emotion
+    
     else:
         return {"error": response.status_code, "message": response.text}
 
